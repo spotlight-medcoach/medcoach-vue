5e0079a365573235e3fe8f7bc61e97df166b8ae1
Angel Ramirez <ramirezcastorena_b@outlook.com>
2020-08-28 18:37:54 -0500

    Fixed style in simulator cards

--- components/simulators/SimulatorCard.vue
+++ components/simulators/SimulatorCard.vue
@@ -3,11 +3,11 @@
  :class="['simulator',
   simulator.finished ? 'finalizado' : 'pendiente'
  ]">
-    <div class="d-flex">
+    <div class="d-flex align-items-center justify-content-between">
         <div class="content">
-        <h3 :class="simulator.finished ? 'text-finalizado' : 'text-pendiente'">Exámenes simuladores</h3>
-        <h4 v-if="simulator.finished">Finalizado</h4>
-        <h4 style="color:#fff" v-else>Pendiente</h4>
+          <h3 :class="simulator.finished ? 'text-finalizado' : 'text-pendiente'">Exámenes simuladores</h3>
+          <h4 v-if="simulator.finished">Finalizado</h4>
+          <h4 style="color:#fff" v-else>Pendiente</h4>
         </div>
         <b-button class="buttontest" v-if="simulator.finished === false" v-on:click="gotoSimulator">Tomar examen</b-button>
         <b-button class="buttonfeedback" v-if="simulator.finished === true" v-on:click="gotoResults">Ver retroalimentación</b-button>
@@ -40,9 +40,7 @@ export default {
 <style>
     .simulator {
         margin: 10px;
-        padding: 5px;
         width:100%;
-        height:100px;
         border-radius:20px;
     }
 
@@ -56,11 +54,10 @@ export default {
     }
 
     .content{
-        margin: 10px 50px 5px 20px;
-        width: 80%;
+        margin: 10px 0px 5px 25px;
     }
     .buttontest{
-        margin: 10px;
+        margin-right: 25px;
         background-color:#FE9400;
         width:175px;
         height:50px;
@@ -72,7 +69,9 @@ export default {
       color:#fff;
     }
     .buttonfeedback{
+      margin-right: 25px;
       height:65px;
+      width:175px;
       background-color: #fff;
       color:#000;
       line-height: 1;
--- pages/introduction_simulator.vue
+++ pages/introduction_simulator.vue
@@ -1,52 +1,44 @@
 <template>
-    <div class="return">
-        <div>
-        <b-button v-on:click="back()" style="background-color:#fff; color:#FE9400;font-size:18px; border-style:none;">
-            <b-icon-chevron-left style="color:#FE9400; width:25px; height:25px;"></b-icon-chevron-left>
-        <b>Regresar</b>
-        </b-button>
-        </div>
-        <div class="title">
-        <h1><b>Sobre los exámenes simuladores</b></h1>
-        </div>
-        <div class="text">
-        <h6>Bienvenido al simulador ENARM</h6>
-
-<h6>Antes de comenzar debes considerar ciertos puntos para contestar tu examen.</h6>
-
-<h6>El formato que se presenta es muy similar a lo que encontrarás el día de tu Nacional, incluyendo la interfaz de usuario.</h6>
-
-<h6>Debes elegir un día donde te encuentres completamente libre, ya que la duración total del examen es alrededor de <b>11 horas.</b></h6>
-
-<h6>El examen se compone de <b>450 reactivos</b>; 405 preguntas en español y 45 en inglés.</h6>
-
-<h6>A su vez, se divide en dos bloques; el <b>bloque 1 tiene 250 reactivos</b> y tendrás un tiempo de </b>5 horas para contestarlo.</b></h6>
-
-<h6>Posteriormente, recibirás un periodo de </b>descanso de 2 horas.</b></h6>
-
-<h6>Después de las cuales iniciara automáticamente el <b>bloque 2 que consta de 200 reactivos</b> donde tendrás <b>4 horas y media</b> para contestarlo.</h6>
-
-<h6>Mucha suerte.</h6>
-        </div>
-        <div class="button">
-        <b-button class="buttontest" v-b-modal.modal-1>Tomar examen</b-button>
-         <b-modal id="modal-1" hide-footer hide-header>
-         <p class="title" style="font-size:24px"><b>Confirmación</b></p>
-         <p class="title">Escribe la palabra <b>ENARM</b> para comenzar tu examen</p>
-         <b-input v-model="enarm"></b-input>
-         <p class="error" v-if="this.error">Error, ingresa la palabra <b>ENARM</b></p>
-         <div style="text-align:center">
-         <b-button center-align class="comenzar" v-on:click="gotoSimulator">Comenzar</b-button>
-         </div>
-         </b-modal>
-         <b-modal id="modal-2" hide-footer hide-header  no-close-on-backdrop no-close-on-esc>
-         <p class="title" style="font-size:24px"><b>Creando simulador, espere</b></p>
-         <div>
-         <img class="image" src="@/assets/simulator_loading.svg" width="70" height="70">
-         </div>
-         </b-modal>
-        </div>
-    </div>
+<div class="return">
+  <div>
+    <b-button v-on:click="back()" style="background-color:#fff; color:#FE9400;font-size:18px; border-style:none;">
+      <b-icon-chevron-left style="color:#FE9400; width:25px; height:25px;"></b-icon-chevron-left>
+      <b>Regresar</b>
+    </b-button>
+  </div>
+  <div class="title">
+    <h1><b>Sobre los exámenes simuladores</b></h1>
+  </div>
+  <div class="text">
+    <h6>Bienvenido al simulador ENARM</h6>
+    <h6>Antes de comenzar debes considerar ciertos puntos para contestar tu examen.</h6>
+    <h6>El formato que se presenta es muy similar a lo que encontrarás el día de tu Nacional, incluyendo la interfaz de usuario.</h6>
+    <h6>Debes elegir un día donde te encuentres completamente libre, ya que la duración total del examen es alrededor de <b>11 horas.</b></h6>
+    <h6>El examen se compone de <b>450 reactivos</b>; 405 preguntas en español y 45 en inglés.</h6>
+    <h6>A su vez, se divide en dos bloques; el <b>bloque 1 tiene 250 reactivos</b> y tendrás un tiempo de </b>5 horas para contestarlo.</b></h6>
+    <h6>Posteriormente, recibirás un periodo de </b>descanso de 2 horas.</b></h6>
+    <h6>Después de las cuales iniciara automáticamente el <b>bloque 2 que consta de 200 reactivos</b> donde tendrás <b>4 horas y media</b> para contestarlo.</h6>
+    <h6>Mucha suerte.</h6>
+  </div>
+  <div class="button">
+    <b-button class="buttontest" v-b-modal.modal-1>Tomar examen</b-button>
+     <b-modal id="modal-1" hide-footer hide-header>
+     <p class="title" style="font-size:24px"><b>Confirmación</b></p>
+     <p class="title">Escribe la palabra <b>ENARM</b> para comenzar tu examen</p>
+     <b-input v-model="enarm"></b-input>
+     <p class="error" v-if="this.error">Error, ingresa la palabra <b>ENARM</b></p>
+     <div style="text-align:center">
+     <b-button center-align class="comenzar" v-on:click="gotoSimulator">Comenzar</b-button>
+     </div>
+     </b-modal>
+     <b-modal id="modal-2" hide-footer hide-header  no-close-on-backdrop no-close-on-esc>
+     <p class="title" style="font-size:24px"><b>Creando simulador, espere</b></p>
+     <div>
+     <img class="image" src="@/assets/simulator_loading.svg" width="70" height="70">
+     </div>
+     </b-modal>
+  </div>
+</div>
 </template>
 <script>
 import moment from 'moment'
@@ -108,10 +100,12 @@ export default {
   }
   .title{
     margin:30px;
+    margin-bottom: 45px;
     text-align:center;
   }
   .text {
-    margin: 50px;
+    width: 80%;
+    margin: auto;
   }
   .buttontest{
     margin: 10px;
@@ -131,10 +125,15 @@ export default {
     color:#FF4444;
   }
   .image{
-     display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 50%;
-  margin-bottom:20px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    margin-bottom:20px;
+  }
+  @media (min-width: 1600px) {
+    .text {
+      width: 60%;
+    }
   }
 </style>
