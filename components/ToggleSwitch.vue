44fe3f8b3728c41a6bd7fbb0ff414aa94a2c4a3e
Angel Ramirez <ramirezcastorena_b@outlook.com>
2022-01-31 23:32:02 -0600

    Init F8-B11-S17-X45 - Formulario de creación de examen

--- components/ToggleSwitch.vue
+++ components/ToggleSwitch.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="d-flex">
+<div class="d-flex toggle-switch">
   <span class="mr-2">{{ label }}</span>
   <label class="switch">
     <input type="checkbox" v-model="myVal" :disabled="disabled">
@@ -35,77 +35,77 @@ export default {
   }
 }
 </script>
-<style>
-  /* The switch - the box around the slider */
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 2.5rem;
-  height: 1.25rem;
-}
+<style lang="scss">
+@import '@/assets/css/variables/color-palette.scss';
+/* The switch - the box around the slider */
+.toggle-switch {
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 2.5rem;
+    height: 1.25rem;
+  }
 
-/* Hide default HTML checkbox */
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
+  /* Hide default HTML checkbox */
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
 
-/* The slider */
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #D4D5D7;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
+  /* The slider */
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #FFF;
+    -webkit-transition: .4s;
+    transition: .4s;
+    border: thin solid #{$neutral-600};
+  }
 
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 1.25rem;
-  width: 1.25rem;
-  left: 0px;
-  bottom: 0px;
-  -webkit-transition: .4s;
-  transition: .4s;
-  border: thin solid #D4D5D7;
-  background-color: #FE9501;
-}
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 1.25rem;
+    width: 1.25rem;
+    left: -1px;
+    bottom: -1px;
+    -webkit-transition: .4s;
+    transition: .4s;
+    border: thin solid #{$neutral-600};
+    background-color: #c7c7c7;
+  }
 
-input:checked + .slider {
-  background-color: #FD9326;
-}
+  input:checked + .slider:before {
+    background-color: #{$main-500};
+    border: thin solid #{$main-500};
+  }
 
-input:checked + .slider:before {
-  background-color: white;
-  border: thin solid #FD9326;
-}
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196F3;
+  }
 
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
-}
+  input:checked + .slider:before {
+    -webkit-transform: translateX(1.25rem);
+    -ms-transform: translateX(1.25rem);
+    transform: translateX(1.25rem);
+  }
 
-input:checked + .slider:before {
-  -webkit-transform: translateX(1.25rem);
-  -ms-transform: translateX(1.25rem);
-  transform: translateX(1.25rem);
-}
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 20px;
+  }
 
-/* Rounded sliders */
-.slider.round {
-  border-radius: 20px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
-}
-.disabled .slider {
-  cursor: default;
-  opacity: 0.8;
+  .slider.round:before {
+    border-radius: 50%;
+  }
+  .disabled .slider {
+    cursor: default;
+    opacity: 0.8;
+  }
 }
 </style>
