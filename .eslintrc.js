ecb7e575b4115e138fb94c31d268706a172ab0bf
Joseph fernandez <josephfernandeza@gmail.com>
2022-08-10 20:14:39 -0400

    refactor: responsive search

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,22 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: '@babel/eslint-parser',
-    "requireConfigFile": false
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
+  extends: ["@nuxtjs", "plugin:nuxt/recommended"],
   // add your custom rules here
   rules: {
-    'unicorn/escape-case': 'off',
-    'no-console': 'off',
-    'no-tabs': 0,
-    'indent': 'off',
+    "unicorn/escape-case": "off",
+    "no-console": "off",
+    "no-tabs": 0,
+    indent: "off",
     // 'indent': ['error', 'tab'],
     // 'vue/html-indent': ['error', 'tab'],
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/multiline-html-element-content-newline': 'off'
-  }
-}
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/multiline-html-element-content-newline": "off",
+  },
+};
